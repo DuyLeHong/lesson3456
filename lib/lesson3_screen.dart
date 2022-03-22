34b853682f2731b0f0de825fe31ae71e5be3a0c6
Duy Le <lehongduy.bk@gmail.com>
2022-03-22 19:57:10 +0700

    StatefulWidget Example

--- lib/lesson3_screen.dart
+++ lib/lesson3_screen.dart
@@ -11,11 +11,25 @@ class MyAppForLesson3 extends StatelessWidget {
 
     var counter = 1;
 
-    return MaterialApp(home: Lesson3HomePage());
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Lesson3HomePage());
   }
 }
 
-class Lesson3HomePage extends StatelessWidget {
+class Lesson3HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<Lesson3HomePage> {
+  int counter = 1;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -23,20 +37,26 @@ class Lesson3HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Title AppBar'),
       ),
-      body: Center(child: Column(
+      body: Center(
+          child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text('Lesson 3 - HomePage'),
-          Text('Lesson 3 - HomePage'),
-          Text('Lesson 3 - HomePage'),
-          Text('Lesson 3 - HomePage'),
-          Text('Lesson 3 - HomePage'),
+          Text('Số lần click vào nút:'),
+          Text(
+            '$counter',
+            style: Theme.of(context).textTheme.headline3,
+          ),
         ],
       )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.plus_one),
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
       ),
     );
   }
