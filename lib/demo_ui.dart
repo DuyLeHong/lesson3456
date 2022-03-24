140726592f646e0b7b72257ff3c3b0a38a51f448
Duy Le <lehongduy.bk@gmail.com>
2022-03-24 21:07:34 +0700

    Some others UI

--- lib/demo_ui.dart
+++ lib/demo_ui.dart
@@ -39,10 +39,56 @@ class Lesson4HomePage extends StatelessWidget {
                 SizedBox(height: 20,),
 
                 _buildStack(),
+
+                SizedBox(height: 20,),
+
+                _buildCard(),
               ],
             ),
           ),
         ));
+  }
+
+  Widget _buildCard() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      height: 210,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text(
+                '1625 Main Street',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: const Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text(
+                '(408) 555-1212',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: const Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildStack() {
