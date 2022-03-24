975f0246e1eb9ccdc478b641e7ffffe29d6f0e9b
Duy Le <lehongduy.bk@gmail.com>
2022-03-24 20:37:37 +0700

    StackWidgetExample

--- lib/StackWidgetExample.dart
+++ lib/StackWidgetExample.dart
@@ -27,24 +27,64 @@ class Lesson4HomePage extends StatelessWidget {
         ),
         body: SafeArea(
           child: Center(
-            child: Stack(
-              alignment: Alignment.topRight,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.topRight,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: 90,
+                      height: 90,
+                      color: Colors.green,
+                    ),
+                    Positioned(
+                      child: Container(
+                        width: 80,
+                        height: 80,
+                        color: Colors.blue,
+                      ),
+                      bottom: 0,
+                      left: 0,
+                    ),
+                  ],
                 ),
+                SizedBox(height: 10,),
                 Container(
-                  width: 90,
-                  height: 90,
-                  color: Colors.green,
+                  width: 250,
+                  height: 250,
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5.0),
+                        alignment: Alignment.bottomCenter,
+                        // decoration: BoxDecoration(
+                        //   gradient: LinearGradient(
+                        //     begin: Alignment.topCenter,
+                        //     end: Alignment.bottomCenter,
+                        //     colors: <Color>[
+                        //       Colors.black.withAlpha(0),
+                        //       Colors.black12,
+                        //       Colors.black45
+                        //     ],
+                        //   ),
+                        // ),
+                        child: const Text(
+                          'Foreground Text',
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                Positioned(child: Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.blue,
-                ), bottom: 0, left: 0,),
               ],
             ),
           ),
