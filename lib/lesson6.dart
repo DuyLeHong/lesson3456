eed1397ba2c6c8271b0d47233d2297a8ca1a12ee
Duy Le <lehongduy.bk@gmail.com>
2022-03-31 19:03:46 +0700

    GridView example

--- lib/lesson6.dart
+++ lib/lesson6.dart
@@ -27,17 +27,69 @@ class Lesson6HomePage extends StatelessWidget {
         ),
         body: SafeArea(
           child: Center(
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                itemCount: 300,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    color: Colors.amber,
-                    child: Center(child: Text('$index')),
-                  );
-                }
+            child: Column(
+              children: [
+                Expanded(
+                    child: CustomScrollView(
+                  primary: false,
+                  slivers: <Widget>[
+                    SliverPadding(
+                      padding: const EdgeInsets.all(20),
+                      sliver: SliverGrid.count(
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        crossAxisCount: 2,
+                        children: <Widget>[
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            child:
+                                const Text("He'd have you all unravel at the"),
+                            color: Colors.green[100],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Text('Heed not the rabble'),
+                            color: Colors.green[200],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Text('Sound of screams but the'),
+                            color: Colors.green[300],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Text('Who scream'),
+                            color: Colors.green[400],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Text('Revolution is coming...'),
+                            color: Colors.green[500],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Text('Revolution, they...'),
+                            color: Colors.green[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+                Expanded(
+                    child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                        ),
+                        itemCount: 300,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            color: Colors.amber,
+                            child: Center(child: Text('$index')),
+                          );
+                        }))
+              ],
             ),
           ),
         ));
